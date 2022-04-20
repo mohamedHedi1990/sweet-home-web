cf0fdcec8e08eba8e1b3c64c489eab7b66be5c07
salima-mn <82147651+salima-mn@users.noreply.github.com>
2022-04-20 20:15:06 +0100

    Update and rename AddressDto.ts to AddressDtoModel.ts

---
+++ src/app/models/dto/AddressDtoModel.ts
@@ -0,0 +1,16 @@
+import {CityDto} from "./CityDtoModel";
+
+export class AddressDtoModel {
+
+  addressStreet:string;
+  addressStreetNumber:number;
+  //String addressCity;
+  addressCity:CityDtoModel;
+
+
+  constructor(addressStreet: string, addressStreetNumber: number, addressCityModel: CityDtoModel) {
+    this.addressStreet = addressStreet;
+    this.addressStreetNumber = addressStreetNumber;
+    this.addressCityModel = addressCityModel;
+  }
+}
