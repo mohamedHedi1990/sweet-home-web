e13091c5d035b6db3ec05e53821d64d9c6a40eab
salima-mn <82147651+salima-mn@users.noreply.github.com>
2022-04-29 20:58:47 +0100

    fixing some bugs + some updating in navbar (#9)

--- src/app/components/dashbord-user/dashbord-user.component.ts
+++ src/app/components/dashbord-user/dashbord-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RoleCode} from "../../enums/role-code";
 
 @Component({
   selector: 'app-dashbord-user',
@@ -10,6 +11,24 @@ export class DashbordUserComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  hasOwnerRole():boolean{
+    if(localStorage.getItem("current-user-role"))
+      if(localStorage.getItem("current-user-role") === RoleCode[0]
+        || localStorage.getItem("current-user-role") === RoleCode[2])
+        return true;
+
+    return false;
+  }
+
+  hasLodgerRole():boolean{
+    if(localStorage.getItem("current-user-role"))
+      if(localStorage.getItem("current-user-role") === RoleCode[0]
+        || localStorage.getItem("current-user-role") === RoleCode[1])
+        return true;
+
+    return false;
   }
 
 }
