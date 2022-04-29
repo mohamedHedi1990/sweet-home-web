6f5b376a9e700d6e78f92b06ca0089a36c55d79b
Salima Mnassri <salimamnassri@gmail.com>
2022-04-29 19:03:13 +0100

    fixing some bugs + some updating in navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AlertErrorComponent } from './components/alert-error/alert-error.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [AlertErrorComponent, NavbarComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, BsDropdownModule.forRoot(), RouterModule],
   exports: [AlertErrorComponent, NavbarComponent, FooterComponent],
 })
 export class SharedModule {}
